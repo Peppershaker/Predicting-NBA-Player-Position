066e59267f6986d0b3e3138e1748060e0ac3910d
victor <victor.c.xu@gmail.com>
2019-06-28 16:10:12 -0400

    pep8 complient

--- src/espn_scraper.py
+++ src/espn_scraper.py
@@ -8,12 +8,20 @@ import numpy as np
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
+
 class espn_player_scraper():
     def _get_team_page_urls(self, teams_overview_url):
+        """Takes ESPN NBA team listing URL and returns a dict
+        mapping team name to its ESPN team URL containing all 
+        of the team's players"""
+        
         a_style = "padding-left:0px;margin:0px;float:left;width:50px;text-decoration:none"
 
-        teams_soup = BeautifulSoup(requests.get(teams_overview_url).content, 'html.parser')
-        team_a_list = teams_soup.findAll('a', {'style':'padding-top:5px;padding-left:0px;'})
+        teams_soup = BeautifulSoup(
+            requests.get(teams_overview_url).content,
+            'html.parser')
+        team_a_list = teams_soup.findAll(
+            'a', {'style': 'padding-top:5px;padding-left:0px;'})
 
         team_urls_dict = dict()
         for a in team_a_list:
@@ -25,19 +33,20 @@ class espn_player_scraper():
 
     def _get_players_form_team_soup(self, team_url):
         """
-        Takes ESPN team url and returns a list of soup objects 
+        Takes ESPN team url and returns a list of soup objects
         each corresponding to a player on the team
         """
         response = requests.get(team_url)
         soup = BeautifulSoup(response.content, 'html.parser')
-        player_table_soup = soup.findAll('tr', attrs={"class":"Table2__tr Table2__tr--lg Table2__even"})
+        player_table_soup = soup.findAll(
+            'tr', attrs={"class": "Table2__tr Table2__tr--lg Table2__even"})
 
         return player_table_soup
 
-
     def _get_players_info(self, player_row_soup):
         """
-        Takes soup object for the player of interest and returns relevant info in a list
+        Takes soup object for the player of interest and returns 
+        player info such as name, position and id in a list
         """
         player_info_str = player_row_soup.text
 
@@ -53,7 +62,7 @@ class espn_player_scraper():
         # Get player position
         position_pattern = re.compile(r"[A-Za-z '.-]+[0-9]+([A-Z]+)[0-9]+")
         position_result = re.search(position_pattern, player_info_str)
-        
+
         if position_result is not None:
             player_position = position_result.group(1)
         else:
@@ -62,14 +71,14 @@ class espn_player_scraper():
         # Get player ESPN id. This id is different than the NBA player id
         player_espn_url = player_row_soup.find('a')['href']
         player_id_result = re.search(re.compile('[0-9]{3,}'), player_espn_url)
-        
+
         if player_id_result is not None:
             player_id = player_id_result.group(0)
         else:
             player_id = np.nan
 
         return [player_name, player_position, player_id, player_espn_url]
-    
+
     def scrape_all_players(self, teams_overview_url):
         """
         Scrape ESPN for player information. Limited to only players active in
@@ -83,7 +92,7 @@ class espn_player_scraper():
             :A data frame containing all players, their espn id, position, and
             ESPN player profile page url
         """
-        
+
         # Scrape all player info
         teams_overview_url = "http://www.espn.com/nba/players"
         team_url_dict = self._get_team_page_urls(teams_overview_url)
@@ -97,6 +106,8 @@ class espn_player_scraper():
                 player_info = self._get_players_info(player_soup)
                 player_scraping_result.append(player_info)
 
-        players_df = pd.DataFrame(player_scraping_result, columns=['name', 'position',  "espn_player_id", 'url'])
+        players_df = pd.DataFrame(
+            player_scraping_result, columns=[
+                'name', 'position', "espn_player_id", 'url'])
 
         return players_df
--- src/player_stat.py
+++ src/player_stat.py
@@ -1,78 +1,84 @@
+import pandas as pd
+from nba_py import player
 import sys
 sys.path.append('/Users/victor/Documents/code/nba_api')
 
-from nba_py import player
-import pandas as pd
 
 class NoDataError(Exception):
-	pass
+    """Error indicating no data is returned by the API, this is usually caused by players not playing enough matches to generate the stats requested"""
+    pass
+
 
 def get_player_stat(player_id):
-	"""
-	Takes player NBA id and returns a data frame containing player performance
-	data such as shooting, blocking, rebound etc...
-	"""
-# Extracting shooting and blocking columns
-	player_shot_block_raw = player.PlayerShootingSplits(player_id).shot_areas()
-	
-	if player_shot_block_raw.shape[0] == 0:
-	    raise NoDataError("No data for {}".format(player_id))
+    """
+    Takes player NBA ID and returns a data frame containing player performance
+    data such as shooting, blocking, rebound etc...
+    """
+    # Extracting shooting and blocking columns
+    player_shot_block_raw = player.PlayerShootingSplits(player_id).shot_areas()
 
-	player_shot_and_block_by_area = player_shot_block_raw.T
-	player_shot_and_block_by_area.columns = player_shot_and_block_by_area.loc['GROUP_VALUE']
-	player_shot_and_block_by_area = player_shot_and_block_by_area.loc[['FGA','BLKA'], "Restricted Area":"Above the Break 3"]	
+    if player_shot_block_raw.shape[0] == 0:
+        raise NoDataError("No data for {}".format(player_id))
 
-	player_shot_by_area = player_shot_and_block_by_area.loc[["FGA"],:]
-	player_block_by_area = player_shot_and_block_by_area.loc[["BLKA"],:]
+    player_shot_and_block_by_area = player_shot_block_raw.T
+    player_shot_and_block_by_area.columns = player_shot_and_block_by_area.loc['GROUP_VALUE']
+    player_shot_and_block_by_area = player_shot_and_block_by_area.loc[[
+        'FGA', 'BLKA'], "Restricted Area":"Above the Break 3"]
 
-	# Use a dict to map old col name to new col name, using dict is required
-	# because the API sometimes returns inconsistent column order and number of cols
-	shot_col_map = {"Restricted Area":'shot_res',
-	               "In The Paint (Non-RA)":'shot_in_paint',
-	               "Mid-Range":'shot_mid_range',
-	               'Left Corner 3':'shot_lcorner_3',
-	               'Right Corner 3':'shot_rcorner_3',
-	               'Above the Break 3':'shot_above_3'}
+    player_shot_by_area = player_shot_and_block_by_area.loc[["FGA"], :]
+    player_block_by_area = player_shot_and_block_by_area.loc[["BLKA"], :]
 
-	block_col_map = {"Restricted Area":'block_res',
-	               "In The Paint (Non-RA)":'block_in_paint',
-	               "Mid-Range":'block_mid_range',
-	               'Left Corner 3':'block_lcorner_3',
-	               'Right Corner 3':'block_rcorner_3',
-	               'Above the Break 3':'block_above_3'}
+    # Use a dict to map old col name to new col name, using dict is required
+    # because the API sometimes returns inconsistent column order and number
+    # of cols
+    shot_col_map = {"Restricted Area": 'shot_res',
+                    "In The Paint (Non-RA)": 'shot_in_paint',
+                    "Mid-Range": 'shot_mid_range',
+                    'Left Corner 3': 'shot_lcorner_3',
+                    'Right Corner 3': 'shot_rcorner_3',
+                    'Above the Break 3': 'shot_above_3'}
 
-	# Applying the column name map
-	player_shot_by_area = player_shot_by_area.rename(columns=shot_col_map)
-	player_block_by_area = player_block_by_area.rename(columns=block_col_map)
+    block_col_map = {"Restricted Area": 'block_res',
+                     "In The Paint (Non-RA)": 'block_in_paint',
+                     "Mid-Range": 'block_mid_range',
+                     'Left Corner 3': 'block_lcorner_3',
+                     'Right Corner 3': 'block_rcorner_3',
+                     'Above the Break 3': 'block_above_3'}
 
-	# Total attempted shots and blocks
-	total_shots = player_shot_by_area.loc['FGA',:].sum()
-	total_blocks = player_block_by_area.loc['BLKA',:].sum()
+    # Applying the column name map
+    player_shot_by_area = player_shot_by_area.rename(columns=shot_col_map)
+    player_block_by_area = player_block_by_area.rename(columns=block_col_map)
 
-	# Normalize all stats by total shots to get relative frequencies for each player
-	player_shot_by_area /= total_shots + 1e-5
-	player_block_by_area /= total_blocks + 1e-5
+    # Total attempted shots and blocks
+    total_shots = player_shot_by_area.loc['FGA', :].sum()
+    total_blocks = player_block_by_area.loc['BLKA', :].sum()
 
-	# Record total attempted shots and blocks
-	player_shot_by_area['fga'] = total_shots
-	player_block_by_area['blka'] = total_blocks
+    # Normalize all stats by total shots to get relative frequencies for each
+    # player
+    player_shot_by_area /= total_shots + 1e-5
+    player_block_by_area /= total_blocks + 1e-5
 
-	# PlayerYearOverYearSplits is the end point we will be calling 
-	# to get player stats aside from shooting and blocking
-	# Take the most recent 2 years, and then add them
-	rebound_assist = player.PlayerYearOverYearSplits(player_id, per_mode="Per48").by_year().iloc[:2,:]
+    # Record total attempted shots and blocks
+    player_shot_by_area['fga'] = total_shots
+    player_block_by_area['blka'] = total_blocks
 
-	cols_of_interest = ['OREB', 'DREB', 'AST', 'STL', "MIN"]
+    # PlayerYearOverYearSplits is the end point we will be calling
+    # to get player stats aside from shooting and blocking
+    # Take the most recent 2 years, and then add them
+    rebound_assist = player.PlayerYearOverYearSplits(
+        player_id, per_mode="Per48").by_year().iloc[:2, :]
 
-	rebound_assist = rebound_assist[cols_of_interest]
-	rebound_assist.columns = [c.lower() for c in rebound_assist.columns]
-	rebound_assist_summed = pd.DataFrame(rebound_assist.sum()).T
+    cols_of_interest = ['OREB', 'DREB', 'AST', 'STL', "MIN"]
 
-	# Finally, concatnate the dataframes together
-	player_stat = pd.concat([player_shot_by_area.reset_index(drop=True), 
-	                            player_block_by_area.reset_index(drop=True), 
-	                            rebound_assist_summed.reset_index(drop=True)],
-	                            axis=1)
-	player_stat = player_stat.rename({0:player_id})
+    rebound_assist = rebound_assist[cols_of_interest]
+    rebound_assist.columns = [c.lower() for c in rebound_assist.columns]
+    rebound_assist_summed = pd.DataFrame(rebound_assist.sum()).T
 
-	return player_stat
+    # Finally, concatnate the dataframes together
+    player_stat = pd.concat([player_shot_by_area.reset_index(drop=True),
+                             player_block_by_area.reset_index(drop=True),
+                             rebound_assist_summed.reset_index(drop=True)],
+                            axis=1)
+    player_stat = player_stat.rename({0: player_id})
+
+    return player_stat
